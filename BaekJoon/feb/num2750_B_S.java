b55ebe8816c39ab013b156b3d23a353ad3d3d5a3
leees00n <leees0053@gmail.com>
2023-02-02 22:23:54 +0900

    TIL

--- BaekJoon/feb/num2750_B_S.java
+++ BaekJoon/feb/num2750_B_S.java
@@ -1,5 +1,36 @@
 package BaekJoon.feb;
 
-public class num2750_B_S {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-}
+public class num2750_B_S {
+    public static void main (String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        //선택 정렬 selection sort
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+            for(int val : arr){
+                sb.append(val).append('\n');
+            }
+            System.out.println(sb);
+        }
+    }
+
